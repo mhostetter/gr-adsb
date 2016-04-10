00e3eeb546fd45cf9c1833810c322058f774e042
mhostetter <mhostetter@users.noreply.github.com>
2016-04-10 15:22:22 -0400

    Added example ADS-B receiver flowgraph.

--- examples/adsb_rx.py
+++ examples/adsb_rx.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Adsb Rx
-# Generated: Fri Apr  8 09:28:00 2016
+# Generated: Sun Apr 10 15:07:57 2016
 ##################################################
 
 if __name__ == '__main__':
@@ -17,16 +17,16 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
+from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
-import osmosdr
+import adsb
 import sip
 import sys
-import time
 
 
 class adsb_rx(gr.top_block, Qt.QWidget):
@@ -57,53 +57,125 @@ class adsb_rx(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.update_rate = update_rate = 60
         self.rf_gain = rf_gain = 14
         self.if_gain = if_gain = 32
-        self.fs_mhz = fs_mhz = 16
-        self.fc_mhz = fc_mhz = 1088
+        self.fs_mhz = fs_mhz = 20
+        self.filename = filename = "/home/matt/adsb_1088MHz_20Msps.fc32"
+        self.fc_mhz = fc_mhz = 1089
         self.bb_gain = bb_gain = 16
 
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_sink_x_0 = qtgui.sink_c(
-        	8192, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	fc_mhz*1e6, #fc
-        	fs_mhz*1e6, #bw
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+        	fs_mhz*500, #size
+        	fs_mhz*1e6, #samp_rate
         	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
+        	1 #number of inputs
         )
-        self.qtgui_sink_x_0.set_update_time(1.0/update_rate)
-        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0.set_y_axis(0, 0.1)
         
-        self.qtgui_sink_x_0.enable_rf_freq(True)
+        self.qtgui_time_sink_x_0_0.set_y_label("Amplitude", "")
         
+        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 1e-6, 0, "burst")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(True)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
         
-          
-        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "hackrf=0,bias=1" )
-        self.osmosdr_source_0.set_sample_rate(fs_mhz*1e6)
-        self.osmosdr_source_0.set_center_freq(fc_mhz*1e6, 0)
-        self.osmosdr_source_0.set_freq_corr(0, 0)
-        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0.set_gain(rf_gain, 0)
-        self.osmosdr_source_0.set_if_gain(if_gain, 0)
-        self.osmosdr_source_0.set_bb_gain(bb_gain, 0)
-        self.osmosdr_source_0.set_antenna("", 0)
-        self.osmosdr_source_0.set_bandwidth(0, 0)
-          
+        if not False:
+          self.qtgui_time_sink_x_0_0.disable_legend()
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	fs_mhz*112 + fs_mhz*2, #size
+        	fs_mhz*1e6, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(0, 0.1)
+        
+        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
+        
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_TAG, qtgui.TRIG_SLOPE_POS, 0.0, 1e-6, 0, "burst")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        
+        if not False:
+          self.qtgui_time_sink_x_0.disable_legend()
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, fs_mhz*1e6,True)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_float*1, "", "burst"); self.blocks_tag_debug_0.set_display(True)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, filename, True)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.adsb_framer_0_0 = adsb.framer(fs_mhz*1e6, 0.01, 0.9)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_0, 0), (self.qtgui_sink_x_0, 0))    
+        self.connect((self.adsb_framer_0_0, 0), (self.blocks_tag_debug_0, 0))    
+        self.connect((self.adsb_framer_0_0, 0), (self.qtgui_time_sink_x_0, 0))    
+        self.connect((self.adsb_framer_0_0, 0), (self.qtgui_time_sink_x_0_0, 0))    
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.adsb_framer_0_0, 0))    
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_complex_to_mag_squared_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "adsb_rx")
@@ -111,48 +183,45 @@ class adsb_rx(gr.top_block, Qt.QWidget):
         event.accept()
 
 
-    def get_update_rate(self):
-        return self.update_rate
-
-    def set_update_rate(self, update_rate):
-        self.update_rate = update_rate
-
     def get_rf_gain(self):
         return self.rf_gain
 
     def set_rf_gain(self, rf_gain):
         self.rf_gain = rf_gain
-        self.osmosdr_source_0.set_gain(self.rf_gain, 0)
 
     def get_if_gain(self):
         return self.if_gain
 
     def set_if_gain(self, if_gain):
         self.if_gain = if_gain
-        self.osmosdr_source_0.set_if_gain(self.if_gain, 0)
 
     def get_fs_mhz(self):
         return self.fs_mhz
 
     def set_fs_mhz(self, fs_mhz):
         self.fs_mhz = fs_mhz
-        self.osmosdr_source_0.set_sample_rate(self.fs_mhz*1e6)
-        self.qtgui_sink_x_0.set_frequency_range(self.fc_mhz*1e6, self.fs_mhz*1e6)
+        self.blocks_throttle_0.set_sample_rate(self.fs_mhz*1e6)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.fs_mhz*1e6)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.fs_mhz*1e6)
+
+    def get_filename(self):
+        return self.filename
+
+    def set_filename(self, filename):
+        self.filename = filename
+        self.blocks_file_source_0.open(self.filename, True)
 
     def get_fc_mhz(self):
         return self.fc_mhz
 
     def set_fc_mhz(self, fc_mhz):
         self.fc_mhz = fc_mhz
-        self.osmosdr_source_0.set_center_freq(self.fc_mhz*1e6, 0)
-        self.qtgui_sink_x_0.set_frequency_range(self.fc_mhz*1e6, self.fs_mhz*1e6)
 
     def get_bb_gain(self):
         return self.bb_gain
 
     def set_bb_gain(self, bb_gain):
         self.bb_gain = bb_gain
-        self.osmosdr_source_0.set_bb_gain(self.bb_gain, 0)
 
 
 def main(top_block_cls=adsb_rx, options=None):
