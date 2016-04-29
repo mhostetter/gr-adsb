cc3ebed53bd23ce1aadac43be9570bb0f3ae0f77
mhostetter <mhostetter@users.noreply.github.com>
2016-04-29 19:33:15 -0400

    Removed CSV-to-KML feature.  It's easier and more robust to query the database to generate the KML file.

--- python/sqlite_to_kml.py
+++ python/sqlite_to_kml.py
@@ -35,33 +35,6 @@ COLOR_LUT = [0x5531ff, 0x42afff, 0x5eedff, 0x70f749, 0xfdae2d] # http://www.colo
 
 FT_PER_METER    = 3.28084
 
-def csv_to_kml(csv_filename, kml_filename):
-    # Read CSV and populate plane dictionary
-    reader = csv.reader(open(csv_filename, "r"))
-    for row in reader:
-        if str(row[0]) == "Date/Time":
-            # This row is a header row, ignore it
-            continue
-
-        if len(row) < 9:
-            # Partial CSV row, discard
-            continue
-
-        time_str    = str(row[0])
-        timestamp   = int(row[1])
-        icao        = str(row[2])
-        callsign    = str(row[3])
-        alt         = float(row[4])
-        speed       = float(row[5])
-        heading     = float(row[6])
-        lat         = float(row[7])
-        lon         = float(row[8])
-
-        add_to_dictionary(time_str, timestamp, icao, callsign, alt, speed, heading, lat, lon)
-
-    # Process dictionary and write KML
-    write_kml(kml_filename)
-
 
 def sqlite_to_kml(db_filename, kml_filename):
     # Read database and plot planes
@@ -201,102 +174,12 @@ def kml_style(alpha, color, width):
     return kml
 
 
-def add_to_dictionary(time_str, timestamp, icao, callsign, alt, speed, heading, lat, lon):
-    if plane_dict.has_key(icao) == False:
-        # Add this plane to the dictionary
-        plane_dict[icao] = dict(callsign=callsign, time=[(timestamp, time_str)], position=[[lon, lat, alt]], heading=[[speed, heading]])
-    else:
-        # Update this plane's log
-        if plane_dict[icao]["callsign"] == "" and callsign != "":
-            plane_dict[icao]["callsign"] = callsign
-
-        plane_dict[icao]["time"].append((timestamp, time_str))
-
-        plane_dict[icao]["position"].append([lon, lat, alt])
-        # If any values aren't set (NaN), set them to the previous value
-        for ii in range(0,len(plane_dict[icao]["position"][-1])):
-            if np.isnan(plane_dict[icao]["position"][-1][ii]) == True:
-                plane_dict[icao]["position"][-1][ii] = plane_dict[icao]["position"][-2][ii]
-
-        plane_dict[icao]["heading"].append([speed, heading])
-        # If any values aren't set (NaN), set them to the previous value
-        for ii in range(0,len(plane_dict[icao]["heading"][-1])):
-            if np.isnan(plane_dict[icao]["heading"][-1][ii]) == True:
-                plane_dict[icao]["heading"][-1][ii] = plane_dict[icao]["heading"][-2][ii]
-
-
-def write_kml(kml_filename):
-    kml = ET.Element("kml")
-    file = ET.Comment(text="Automatically generated using generate_kml.py from the gr-adsb GNU Radio Module.")
-
-    Document = ET.SubElement(kml, "Document")
-    
-    name = ET.SubElement(Document, "name")
-    name.text = "ADS-B Data"
-    
-    Snippet = ET.SubElement(Document, "Snippet")
-    Snippet.text = "Created on %s" % ("Fill this in")
-
-    Folder = ET.SubElement(Document, "Folder")
-    
-    name = ET.SubElement(Folder, "name")
-    name.text = "Planes"
-
-    # Add planes
-    for key in plane_dict:
-        Placemark = ET.SubElement(Folder, "Placemark")
-        name = ET.SubElement(Placemark, "name")
-        name.text = "%s : %s" % (key, plane_dict[key]["callsign"])
-
-        gx_Track = ET.SubElement(Placemark, "gx_Track")
-
-        for ii in range(0,len(plane_dict[key]["time"])):
-            lon = plane_dict[key]["position"][ii][0]
-            lat = plane_dict[key]["position"][ii][0]
-            alt = plane_dict[key]["position"][ii][0]
-
-            if np.isnan(lon) == False and np.isnan(lat) == False and np.isnan(alt) == False:
-                when = ET.Element("when")
-                when.text = plane_dict[key]["time"][ii][1]
-
-                gx_coord = ET.Element("gx_coord")
-                gx_coord.text = "%s %s %s" % (
-                            "{:12.8f}".format(lon),
-                            "{:12.8f}".format(lat),
-                            "{:6.1f}".format(alt),
-                        )
-
-                gx_Track.append(when)
-                gx_Track.append(gx_coord)
-
-
-    # ET.ElementTree.write(file, encoding="us-ascii", xml_declaration=True, default_namespace=None, method="xml")
-
-    # ET.dump(kml)
-    # print ET.tostring(kml, pretty_print=True)
-    xml_str = ET.tostring(kml)
-
-    file = open(kml_filename, "w")
-    file.write(xml_str)
-    file.close()
-
-
 if __name__ == "__main__":
     # Set up the command-line arguments
     parser = argparse.ArgumentParser(description="Generate a Google Earth KML file from logged ADS-B data.")
-    parser.add_argument("--type", metavar="type", type=str, nargs="+", help="The input file type (csv, sqlite)")
-    parser.add_argument("--file", metavar="file", type=str, nargs="+", help="The input filename")
+    parser.add_argument("--db_file", metavar="db_file", type=str, default="adsb.sqlite", help="The input SQLite filename")
     parser.add_argument("--kml_file", metavar="kml_file", type=str, default="adsb.kml", help="The output KML filename")
 
     args = parser.parse_args()
 
-    if args.type[0] == "csv":
-        print "Reading from CSV file %s" % (args.file[0])
-        csv_to_kml(args.file[0], args.kml_file)
-
-    elif args.type[0] == "sqlite":
-        print "Reading from SQLite database %s" % (args.file[0])
-        sqlite_to_kml(args.file[0], args.kml_file)
-
-    else:
-        print "Invalid argument %s" % (args.type[0])
+    sqlite_to_kml(args.db_file, args.kml_file)
