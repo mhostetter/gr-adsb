649311397d2f95c31196ea113a4e184973b9d86f
Matt Hostetter <matthostetter@gmail.com>
2017-03-31 16:07:49 -0400

    Update copyright

--- python/qa_decoder.py
+++ python/qa_decoder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2016 Matt Hostetter.
+# Copyright 2016-2017 Matt Hostetter.
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
