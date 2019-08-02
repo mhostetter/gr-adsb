c0f6ffd0c69424d8827bda88889bfe939fc1c46a
mhostetter <matthostetter@gmail.com>
2019-08-01 20:33:31 -0400

    Port webpage from Google Maps to leaflet
    #27

--- web/static/js/map.js
+++ web/static/js/map.js
@@ -1,294 +1,205 @@
 //
-// Copyright 2016-2017 Matt Hostetter.
-// 
+// Copyright 2016-2019 Matt Hostetter.
+//
 // This is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 3, or (at your option)
 // any later version.
-// 
+//
 // This software is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this software; see the file COPYING.  If not, write to
 // the Free Software Foundation, Inc., 51 Franklin Street,
 // Boston, MA 02110-1301, USA.
-// 
+//
 
-var locationInfoWindow; // Global info window, only one can be displayed at a time
-var planeMarkers = new Array(); // Global array of plane markers
-// var planeMarkers = new google.maps.MVCArray();
-var planeInfoWindows = new Array();
-var planeLatLngs = new Array(); // Global array of array of plane locations, used to draw flight paths
-var planePolyLines = new Array(); // Global array of arrary of plane flight path lines
+var planes = {};
 
-// Array of colors in a gradient of red (index 0) to blue (index N-1)
-var colorGradient = [
-  '#FF0004',
-  '#FE1300',
-  '#FE2B00',
-  '#FD4300',
-  '#FD5B00',
-  '#FD7300',
-  '#FC8A00',
-  '#FCA200',
-  '#FBBA00',
-  '#FBD200',
-  '#FBE900',
-  '#F4FA00',
-  '#DCFA00',
-  '#C4F900',
-  '#ACF900',
-  '#94F900',
-  '#7CF800',
-  '#64F800',
-  '#4DF800',
-  '#35F700',
-  '#1DF700',
-  '#06F600',
-  '#00F611',
-  '#00F628',
-  '#00F53F',
-  '#00F556',
-  '#00F56E',
-  '#00F485',
-  '#00F49C',
-  '#00F3B2',
-  '#00F3C9',
-  '#00F3E0',
-  '#00EEF2',
-  '#00D6F2',
-  '#00BFF1',
-  '#00A8F1',
-  '#0091F1',
-  '#0079F0',
-  '#0062F0',
-  '#004BEF'
+// Create SocketIO instance
+var socket = io('http://localhost:5000');
+
+socket.on('connect', function() {
+  console.log('Client has connected via SocketIO.');
+});
+socket.on('disconnect', function() {
+  console.log('Client disconnected via SocketIO.');
+});
+socket.on('updatePlane', function(plane) {
+  updatePlane(map, plane);
+});
+
+// Create the leaflet map
+var map = L.map('map');
+
+// Attempt to locate user. Map will also center to first plane, once received.
+map.locate({setView: true});
+
+// Load various tiles
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+var CartoDB_VoyagerLabelsUnder = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+// Set default tile set
+CartoDB_VoyagerLabelsUnder.addTo(map);
+
+// Add layer/tile control
+var baseMaps = {
+  'OpenStreetMaps': OpenStreetMap_Mapnik,
+  'CartoDB Voyager': CartoDB_VoyagerLabelsUnder,
+  'CartoDB Positron': CartoDB_Positron,
+  'CartoDB Dark Matter': CartoDB_DarkMatter,
+  'ESRI Gray': Esri_WorldGrayCanvas,
+  'ESRI Topo': Esri_WorldTopoMap,
+  'ESRI World Imagery': Esri_WorldImagery,
+  'Stamen Toner Lite': Stamen_TonerLite
+};
+var overlayMaps = {};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// Colormap of altitudes in increments of 1000 ft (Rainbow)
+var colormapRainbow = [
+  '#ff0000', '#ff1209', '#ff2512', '#ff3b1d', '#ff4d27', '#ff5f30', '#ff733b', '#ff8344', '#ff954e',
+  '#ffa457', '#ffb260', '#f2c16a', '#e6cd73', '#d8d97c', '#cce284', '#c0ea8c', '#b2f295', '#a6f79d',
+  '#98fba5', '#8cfeac', '#80feb3', '#72febb', '#66fbc1', '#58f7c8', '#4cf2ce', '#40ecd3', '#32e2d9',
+  '#26d9de', '#18cde3', '#0cc1e7', '#00b4eb', '#0da4ef', '#1995f2', '#2783f5', '#3373f8', '#3f61fa',
+  '#4d4dfb', '#593bfd', '#6725fe', '#7312fe', '#7f00ff'
 ];
+colormap = colormapRainbow;
 
-function initialize() {
-  var grey_styles = [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}];
-  grey_styles.push(
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {"visibility": "simplified"},
-      ]
-    }
-  );
-  grey_styles.push(
-    {
-      "featureType": "transit.station.airport",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {"color": "#000000"},
-        {"lightness": 50},
-        {"saturation": 0},
-      ]
-    }
-  );
+var planeIcon = L.icon({
+  iconUrl: './img/airliner.png',
+  iconSize:     [20, 20],
+});
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.8976451, lng: -77.0367452},
-    zoom: 8,
-    styles: grey_styles
-  });
+// // Adjust marker size based on zoom level
+// var planeSize = [50, 50, 50, 50, 50, 50, 50, 40, 30, 20, 10, 10, 10, 10, 10, 10, 10, 10];
 
-  map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-  // map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-  // map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
-  // map.setMapTypeId(google.maps.MapTypeId.HYBRID);
-  
-
-  // Try HTML5 geolocation
-  locationInfoWindow = new google.maps.InfoWindow({map: map});
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      locationInfoWindow.setPosition(pos);
-      locationInfoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, locationInfoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, locationInfoWindow, map.getCenter());
-  }
-
-  // Create SocketIO instance
-  var socket = io('http://0.0.0.0:5000');
-  
-  socket.on('connect', function() {
-    console.log('Client has connected via SocketIO.');
-  });
-  socket.on('disconnect', function() {
-    console.log('Client disconnected via SocketIO.');
-  });
-  socket.on('updatePlane', function(plane) {
-    updatePlane(map, plane);
-  });
-
-  // google.maps.event.addDomListener(window, 'load', initialize);
-}
+// map.on('zoomend', function() {
+//   var currentZoom = map.getZoom();
+//   console.log('Zooming ' + currentZoom);
+//   planeIcon.iconSize = planeSize[currentZoom];
+// });
 
 
 function updatePlane(map, plane) {
-  if (planeMarkers[plane.icao] == undefined) {
-    // Add plane marker to the map
+  if (planes[plane.icao] == undefined) {
     addPlane(map, plane);
   }
   else {
-    // Move plane marker and update path
     movePlane(map, plane);
   }
 }
 
 
 function addPlane(map, plane) {
-  // Set the initial altitude of the plane
-  color = getAltitudeColor(plane.altitude);
-
-  // Set the initial heading of the plane
-  rotation = getRotation(plane.heading);
-
-  var arrow = {
-    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-    fillColor: color,
-    fillOpacity: 1,
-    strokeColor: 'black',
-    strokeOpacity: 1.0,
-    strokeWeight: 1.5,
-    rotation: rotation,
-    scale: 4
-  };
-
-  var location = new google.maps.LatLng(plane.latitude, plane.longitude);
-
-  // Add new plane marker
-  planeMarkers[plane.icao] = new google.maps.Marker({
-    position: location,
-    icon: arrow,
-    title: plane.callsign,
-    map: map
-  });
-
-  // Create info window
-  planeInfoWindows[plane.icao] = new google.maps.InfoWindow({
-    content: getInfoString(plane)
-  });
-
-  // Initialize an array of locations for this plane
-  planeLatLngs[plane.icao] = new Array();
-  planePolyLines[plane.icao] = new Array();
-  planeLatLngs[plane.icao][0] = {lat: Number(plane.latitude), lng: Number(plane.longitude)};
-
-  google.maps.event.addListener(planeMarkers[plane.icao], 'click', function() {
-    planeInfoWindows[plane.icao].open(map, this);
-  });
+  latlng = [plane.latitude, plane.longitude];
+  // Set initial view of map on first plane reception
+  if (Object.keys(planes).length == 0) {
+    map.setView(latlng, 9);
+  }
+  planes[plane.icao] = {};
+  planes[plane.icao]['marker'] = L.marker(latlng, {
+    icon: planeIcon,
+    rotationAngle: headingToRotationAngle(plane.heading),
+    rotationOrigin: 'center center'
+  }).addTo(map);
+  planes[plane.icao]['tooltip'] = L.tooltip(formatTooltip(plane));
+  planes[plane.icao]['popup'] = L.popup(formatPopup(plane));
+  planes[plane.icao]['marker'].bindTooltip(planes[plane.icao]['tooltip']);
+  planes[plane.icao]['marker'].bindPopup(planes[plane.icao]['popup']);
+  planes[plane.icao]['track'] = L.layerGroup();
+  planes[plane.icao]['last_location'] = latlng;
 }
 
 
 function movePlane(map, plane) {
-  // Add new plane location to paths array
-  position_idx = planeLatLngs[plane.icao].length;
-  planeLatLngs[plane.icao][position_idx] = {lat: Number(plane.latitude), lng: Number(plane.longitude)};
-
-  // Get the altitude color
-  color = getAltitudeColor(plane.altitude);
-
-  // Draw new line between the previous location and current location
-  planePolyLines[plane.icao][position_idx-1] = new google.maps.Polyline({
-    path: [planeLatLngs[plane.icao][position_idx-1], planeLatLngs[plane.icao][position_idx]],
-    geodesic: true,
-    strokeColor: color,
-    strokeOpacity: 1.0,
-    strokeWeight: 3,
-    map: map
-  });
-
-  var icon = planeMarkers[plane.icao].getIcon();
-
-  // Update the plane's heading
-  icon.rotation = getRotation(plane.heading);
-  icon.fillColor = color;
-  planeMarkers[plane.icao].setIcon(icon);
-
-  // Move plane marker to new location
-  var new_location = new google.maps.LatLng(plane.latitude, plane.longitude);
-  planeMarkers[plane.icao].setPosition(new_location);
-  planeMarkers[plane.icao].setTitle(plane.callsign);
-  planeInfoWindows[plane.icao].setContent(getInfoString(plane));
+  latlng = [plane.latitude, plane.longitude];
+  planes[plane.icao]['marker'].setLatLng(latlng);
+  planes[plane.icao]['marker'].setRotationAngle(headingToRotationAngle(plane.heading));
+  planes[plane.icao]['tooltip'].setContent(formatTooltip(plane));
+  planes[plane.icao]['popup'].setContent(formatPopup(plane));
+  prev_latlng = planes[plane.icao]['last_location']
+  planes[plane.icao]['track'].addLayer(L.polyline([prev_latlng, latlng], {color: altitudeColor(plane.altitude)}).addTo(map));
+  planes[plane.icao]['last_location'] = latlng;
 }
 
 
-// function removePlane(map, plane) {
-//   if (planeMarkers[plane.icao] != undefined) {
-//     // Remove plane from map
-//     console.log('Need to remove this plane.', plane);
-
-//     // Delete all polylines
-//     for (var idx = 0; idx < planePolyLines[plane.icao].length; idx++) {
-//       planePolyLines[plane.icao].setMap(null);
-//     }
-
-//     // Delete plane marker
-//     planeMarkers[plane.icao].setMap(null);
-//   }
-// }
-
-
-function getAltitudeColor(altitude) {
-  if (altitude != undefined && altitude != -1) {
-    idx = Math.floor(altitude/1000);
-    if (idx >= colorGradient.length) {
-      idx = colorGradient.length - 1;
-    }
-
-    color = colorGradient[idx];
-  }
-  else {
-    color = 'black';
-  }
-
-  return color;
+function formatTooltip(plane) {
+  return plane.icao + ': ' + plane.callsign;
 }
 
 
-function getRotation(heading) {
-  rotation = -1*heading + 90.0;
-  if (rotation < 0) {
-    rotation += 360;
-  }
-
-  return rotation;
-}
-
-
-function getInfoString(plane) {
+function formatPopup(plane) {
   str = '<table>';
   str += '<tr><td><b>ICAO</b></td><td>' + plane.icao + '</td></tr>';
   str += '<tr><td><b>Callsign</b></td><td><a href=\"http://flightaware.com/live/flight/' + plane.callsign + '\" target=\"_blank\">' + plane.callsign + '</a></td></tr>';
+  str += '<tr><td><b>Datetime</b></td><td>' + plane.datetime + '</td></tr>';
   str += '<tr><td><b>Altitude</b></td><td>' + plane.altitude + ' ft</td></tr>';
   str += '<tr><td><b>Vertical Rate</b></td><td>' + plane.vertical_rate + ' ft/min</td></tr>';
   str += '<tr><td><b>Speed</b></td><td>' + plane.speed.toFixed(0) + ' kt</td></tr>';
   str += '<tr><td><b>Heading</b></td><td>' + plane.heading.toFixed(0) + ' deg</td></tr>';
-  str += '<tr><td><b>Latitude</b></td><td>' + plane.latitude.toFixed(4) + '</td></tr>';
-  str += '<tr><td><b>Longitude</b></td><td>' + plane.longitude.toFixed(4) + '</td></tr>';  
+  str += '<tr><td><b>Latitude</b></td><td>' + plane.latitude.toFixed(8) + '</td></tr>';
+  str += '<tr><td><b>Longitude</b></td><td>' + plane.longitude.toFixed(8) + '</td></tr>';
   str += "</table>"
 
   return str;
 }
 
 
-function handleLocationError(browserHasGeolocation, locationInfoWindow, pos) {
-  locationInfoWindow.setPosition(pos);
-  locationInfoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+function headingToRotationAngle(heading) {
+  return -heading;
+}
+
+
+function altitudeColor(altitude) {
+  if (altitude != undefined && altitude != -1) {
+    idx = Math.floor(altitude / 1000);
+    if (idx < 0) {
+      idx = 0;
+    }
+    else if (idx >= colormap.length) {
+      idx = colormap.length - 1;
+    }
+    return colormap[idx];
+  }
+  else {
+    return 'black';
+  }
 }
